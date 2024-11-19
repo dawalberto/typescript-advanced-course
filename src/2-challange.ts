6bb4672d68867e23c4dcd0a60e1e37e247023737
dawalberto <albertoinformatica97@gmail.com>
2024-11-19 20:12:11 +0100

    challenge 1 and 2 solved

---
+++ src/2-challange.ts
@@ -0,0 +1,21 @@
+import { Expect, Equal } from ".";
+
+namespace merge {
+  function merge<T, K>(a: T, b: K): T & K {
+    return { ...a, ...b };
+  }
+
+  const res1 = merge({ name: "Bob" }, { age: 42 });
+  type test1 = Expect<Equal<typeof res1, { name: string } & { age: number }>>;
+
+  const res2 = merge({ greeting: "Hello" }, {});
+  type test2 = Expect<Equal<typeof res2, { greeting: string }>>;
+
+  const res3 = merge({}, { greeting: "Hello" });
+  type test3 = Expect<Equal<typeof res3, { greeting: string }>>;
+
+  const res4 = merge({ a: 1, b: 2 }, { c: 3, d: 4 });
+  type test4 = Expect<
+    Equal<typeof res4, { a: number; b: number } & { c: number; d: number }>
+  >;
+}
